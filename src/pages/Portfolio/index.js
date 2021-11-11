cd62d91e895a76b934dc73d5e8a058f31733243f
SC2021 <walterlaw19@gmail.com>
2021-11-10 21:12:09 -0500

    update to six projects

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -18,77 +18,67 @@ function Portfolio() {
         <>
             <section id="work" class="projects">
                 <div class="section-h2">
-                    <h2>My Work</h2>
+                    {/* <h2>My Work</h2> */}
                 </div>
 
                 <div class="website">
 
-                    <div id="nptp" class="main-website">
-                        <a href="https://miraj00.github.io/National-Park-Trip-Planner/" target="blank" >National Park Trip Planner </a>
+                    {/* <div id="nptp" class="main-website">
+                        <a href="https://miraj00.github.io/National-Park-Trip-Planner/" target="blank" >Nationalll Park Trip Planner </a>
                     </div>
 
                     <div id="dream-home" class="main-website">
                         <a href="https://sheltered-eyrie-12242.herokuapp.com/" target="blank" >Dream Home</a>
                     </div>
+                    <div>
+                        weather
+                    </div> */}
 
 
                     <div class="row-websites">
                         {/* <div id="coderefactorpic" class="rows">
                             <a href="https://walterlaw19.github.io/CodeRefactor/" target="blank" title="Refactor"> CodeRefactor</a>
                         </div> */}
-                        <Project id={"coderefactorpic"} link={"https://walterlaw19.github.io/CodeRefactor/"} title={"CodeRefactor"}/>
+                        <Project id={"nptp"} link={"https://miraj00.github.io/National-Park-Trip-Planner/"} title={"National-Park-Trip-Planner"} />
 
 
                         {/* <div id="password-generator" class="rows">
                             <a href="https://walterlaw19.github.io/Password-generator/" target="blank" title="Password Generator">Password Generator</a>
                         </div> */}
-                        <Project id={"password-generator"} link={"https://walterlaw19.github.io/Password-generator/"} title={"Password Generator"}/>
+                        <Project id={"dream-home"} link={"https://sheltered-eyrie-12242.herokuapp.com/"} title={"Dream Home"} />
                     </div>
 
 
                     <div class="row-websites">
-                        <div id="code-quiz" class="rows">
+
+                        {/* <div id="code-quiz" class="rows">
                             <a href="https://walterlaw19.github.io/code-quiz/" target="blank" title="Code Quiz">Code Quiz</a>
-                        </div>
+                        </div> */}
+                        <Project id={"budget-tracker"} link={"https://shrieking-crypt-06375.herokuapp.com/"} title={"budget-tracker"} />
 
 
-                        <div id="work-day-scheduler" class="rows">
+                        {/* <div id="work-day-scheduler" class="rows">
                             <a href="https://walterlaw19.github.io/Work-Day-Scheduler/" target="blank" title="Work Day Scheduler">Work-Day-Scheduler</a>
-                        </div>
+                        </div> */}
+                        <Project id={"tech-blog"} link={"https://walt-first-tech-blog.herokuapp.com/"} title={"tech-blog"} />
+
+
                     </div>
 
                     <div class="row-websites">
-                        <div id="weather-dashboard" class="rows">
+                        <Project id={"employee-tracker"} link={"https://shrieking-crypt-06375.herokuapp.com/"} title={"employee-tracker"} />
+
+                        {/* <div id="weather-dashboard" class="rows">
                             <a href="https://walterlaw19.github.io/weather-dashboard/" target="blank" title="weather-dashboard">Weather Dashboard</a>
-                        </div>
+                        </div> */}
+                        <Project id={"weather"} link={"https://walterlaw19.github.io/weather-dashboard/"} title={"weather"} />
 
 
-                        <div id="fathercubillas" class="rows">
-                            <a href="">Family Socials</a>
-                        </div>
+                        {/* <div id="fathercubillas" class="rows">
+                            <a href="https://shrieking-crypt-06375.herokuapp.com/" target="_blank" title="budget-tracker">Budget Tracker</a>
+                        </div> */}
                     </div>
 
-                    <div class="row-websites">
-                        <div id="waltersinging" class="rows">
-                            <a href="">Voice Major</a>
-                        </div>
-
-
-                        <div id="fathercubillas" class="rows">
-                            <a href="">Family Socials</a>
-                        </div>
-                    </div>
-
-                    <div class="row-websites">
-                        <div id="waltersinging" class="rows">
-                            <a href="">Voice Major</a>
-                        </div>
-
-
-                        <div id="lucassoccer" class="rows">
-                            <a href="">Soccer Coach</a>
-                        </div>
-                    </div>
                 </div>
             </section>
         </>
